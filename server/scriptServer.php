7963911ed7e0041683c26c5864528d036d79da06
rosivancardosoferreira <rosivancardoso767@gmail.com>
2021-09-23 19:17:04 -0400

    feat: New suitable fields, updated presentation text

--- server/scriptServer.php
+++ server/scriptServer.php
@@ -1,8 +1,12 @@
 <?php
 	function getConnection(){
-		$dns = 'mysql:host=banco.ufam.edu.br;dbname=dom_sncticet'; //DADOS DO SERVIDOR ONLINE
-		$user = 'dom_sncticet'; //DADOS DO SERVIDOR ONLINE
-		$pass = 'TB3rzVT8P7'; //DADOS DO SERVIDOR ONLINE
+		// $dns = 'mysql:host=banco.ufam.edu.br;dbname=dom_sncticet'; //DADOS DO SERVIDOR ONLINE
+		// $user = 'dom_sncticet'; //DADOS DO SERVIDOR ONLINE
+		// $pass = 'TB3rzVT8P7'; //DADOS DO SERVIDOR ONLINE
+
+		$dns = 'mysql:host=192.168.0.2;dbname=dom_sncticet'; //DADOS DO SERVIDOR ONLINE
+		$user = 'root'; //DADOS DO SERVIDOR ONLINE
+		$pass = ''; //DADOS DO SERVIDOR ONLINE
 
 		try {
 			$pdo = new PDO($dns, $user, $pass); //Conectado						
